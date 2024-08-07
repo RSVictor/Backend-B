7e2c9c37aa72c73204ec3415c0a4a1c58c45a179
RSVictor <=>
2024-08-07 19:40:46 -0300

    Exemplos javascript variaveis

---
+++ Aula2/exe1.js
@@ -0,0 +1,10 @@
+const idade = 28; // Declara uma constante idade
+const nome = "Victor"; // Declara uma constante nome
+
+console.log("Olá Mundo - Backend");
+console.log(`${idade} ${nome}`); // Exibe no terminal as
+
+const inf =nome + idade;
+
+console.log(inf);
+
