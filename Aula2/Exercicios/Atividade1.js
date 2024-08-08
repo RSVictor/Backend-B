43f4c50636183402323f270f054bc35423f515bf
RSVictor <=>
2024-08-07 22:27:50 -0300

    Adicionado uma pasta contendo atividade 1,2,3.js

---
+++ Aula2/Exercicios/Atividade1.js
@@ -0,0 +1,7 @@
+const marca = "Gol";
+const ano = 2018;
+const vendido = true
+
+console.log(marca, `${typeof marca}`);
+console.log(ano , `${typeof ano}`);
+console.log(vendido , `${typeof vendido}`);
---
+++ Aula2/Exercicios/Atividade2.js
@@ -0,0 +1,9 @@
+const nome = "Victor";
+const ultimoNome = "Ribeiro";
+const nomeesobrenome = nome + ultimoNome;
+const juntos= `${nome} ${ultimoNome}`;
+
+console.log(nome);
+console.log(ultimoNome);
+console.log(nomeesobrenome);
+console.log(juntos);
