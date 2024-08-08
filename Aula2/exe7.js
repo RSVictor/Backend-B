2a838350284c8fae41b0bd34be9176ba3b2a0ce5
RSVictor <=>
2024-08-07 22:17:45 -0300

    Adicionado conteudo no exe7.js

--- Aula2/exe7.js
+++ Aula2/exe7.js
@@ -1,5 +1,6 @@
+/*
 let readline = require(`readline`);
-// CRia uma interface de leitura
+// Cria uma interface de leitura
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -14,3 +15,28 @@ rl.question(`Digite algo: `, (answer) => {
     rl.close();
 });
 console.log(readline);
+
+*/
+
+const readline = require(`readline`);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+let res;
+//Pergunta o primeiro valor
+rl.question('Digtite o primeiro valor: ',(valor1)=>{
+    //Pergunta o segunfo valor
+    rl.question('Digite o segundo valor: ',(valor2) =>{
+        //Exibe os valores digitados
+        console.log('Primeiro valor:', valor1);
+        console.log('Segundo valor:',valor2);
+        console.log(valor1+valor2);
+        console.log(Number(valor1)+Number(valor2));
+
+
+    //Fecha a interface
+    rl.close();
+});
+});
