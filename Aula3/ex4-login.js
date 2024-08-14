e291c9f56614b355c5fd0c50646c27fff6c18944
RSVictor <victorribeiro304@hotmail.com>
2024-08-14 20:05:43 -0300

    Adicionado o exercicio 4 login

---
+++ Aula3/ex4-login.js
@@ -0,0 +1,13 @@
+const login = "senai"; // varivael de login
+const senha = "CFP501"; // variavel de senha
+const op = 1; // variavel de opção
+
+if(login === "senai" && senha === "CFP501" && op === 1){
+    console.log("Bibliotecaria");
+}
+if(login === "senai" && senha === "CFP501" && op === 2){
+console.log("Cliente");
+}
+else{
+    console.log("Opção inexistente");
+}
