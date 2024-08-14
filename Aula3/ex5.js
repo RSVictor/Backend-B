76dc8a5c7a2b5553fbd8c8f136600d08de5cf32f
RSVictor <victorribeiro304@hotmail.com>
2024-08-14 20:45:19 -0300

    Adicionado exercicio 5

---
+++ Aula3/ex5.js
@@ -0,0 +1,13 @@
+const num = "5";
+const valor = null;
+const valor2 = undefined;
+
+const price = "100.50";
+let res = parseFloat(price) + 50;
+let res1 = Number(price) + 50;
+console.log(parseInt(num)); // converte o valor com parse int de string para inteiro
+console.log(Number(valor));// converte o valor ultilizando number // converte paara numero
+console.log(Number(valor2));// converte o valor ultilizando number
+console.log(parseFloat(price)); // converte o valor para decimal
+console.log(res)
+console.log(res1);
