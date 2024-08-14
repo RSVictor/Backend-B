8b774611031b4da9e760a4586fcb8dfd59a72c1f
RSVictor <victorribeiro304@hotmail.com>
2024-08-14 19:33:58 -0300

    adciionado itens

---
+++ Aula3/ex1-string.js
@@ -0,0 +1,18 @@
+const estudante = "Victor"; // variavel string
+let num = 5; // variavel numerica
+let texto = estudante + "" + num; // adiciona a variavel estudante com a variavel num
+let stexto =`nome ${estudante} numero ${num}`; // exemplo template string
+console.log(stexto);
+
+const cifrao = '\u0024'; // simbolo ultilizando codigo utf
+const aMaiusculo = '\u0041'; // simbolo ultilizando codigo utf
+const tique = '\u2705';// simbolo ultilizando codigo utf
+const hiragana = '\u3041';// simbolo ultilizando codigo utf
+let tel_estudante = null;// variavel com valor vazio
+let cpf = undefined;
+console.log(cpf);
+console.log(cifrao);
+console.log(aMaiusculo);
+console.log(tique);
+console.log(hiragana);
+
