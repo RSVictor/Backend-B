e207486426fa5f40a4aadeb40fe247fab613d092
RSVictor <victorribeiro304@hotmail.com>
2024-08-14 20:45:55 -0300

    Adicionado os exercicios 6 e 7

---
+++ Aula3/ex6.js
@@ -0,0 +1,6 @@
+
+//Exemplo valor ternario
+
+const valor = 100;
+let res = valor > 50 ? "Valor maior que 50" : "Valor menor que 50";
+console.log(res);
---
+++ Aula3/ex7.js
@@ -0,0 +1,6 @@
+const texto = "senai backend aula 3";
+let stexto = texto.toUpperCase(); //converte com o texto para maiusculo
+console.log(stexto);
+
+const texto2 = "SENAI DESENVOLVIMENTO DE SISTEMAS";
+console.log(texto2.toLowerCase()) // converte o texto para minusculo
