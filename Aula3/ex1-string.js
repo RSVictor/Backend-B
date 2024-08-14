3142ea12f810c43617a05618a7aee31002c55c4d
RSVictor <victorribeiro304@hotmail.com>
2024-08-14 19:44:49 -0300

    Adicionado codigos

--- Aula3/ex1-string.js
+++ Aula3/ex1-string.js
@@ -2,6 +2,7 @@ const estudante = "Victor"; // variavel string
 let num = 5; // variavel numerica
 let texto = estudante + "" + num; // adiciona a variavel estudante com a variavel num
 let stexto =`nome ${estudante} numero ${num}`; // exemplo template string
+console.log(texto)
 console.log(stexto);
 
 const cifrao = '\u0024'; // simbolo ultilizando codigo utf
@@ -10,9 +11,12 @@ const tique = '\u2705';// simbolo ultilizando codigo utf
 const hiragana = '\u3041';// simbolo ultilizando codigo utf
 let tel_estudante = null;// variavel com valor vazio
 let cpf = undefined;
+
 console.log(cpf);
 console.log(cifrao);
 console.log(aMaiusculo);
 console.log(tique);
 console.log(hiragana);
+console.log(stexto.length);// nome a string . lenght retorna o tamanho da string
+console.log(texto.length);// nome a string . lenght retorna o tamanho da string
 
