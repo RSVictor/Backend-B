40e1151a5c0f15bfdc7f65c675bcc2d22450ec84
RSVictor <152220391+RSVictor@users.noreply.github.com>
2024-08-28 19:14:12 -0300

    Criado pasta aula 5 e adicionado exercicio 1 e 2

---
+++ Aula5/ex1-array.js
@@ -0,0 +1,7 @@
+const frutas = ['Pera','Uva','Maça']; // Cria um array chamado frutas
+console.log(frutas);
+console.log(frutas[2]);
+
+const frutas2 = new Array('Maça','Morango','Laranja') // segunda forma de criar array
+console.log(frutas2)
+console.log(frutas2[1])
---
+++ Aula5/ex2-array.js
@@ -0,0 +1,6 @@
+notas = [5,10,7.5]
+console.log(notas)
+notas.pop(); // remove um elemento do array
+console.log(notas)
+notas.push(9); // Adiciona elementos no array
+console.log(notas)
