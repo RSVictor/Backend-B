848c7e3b7e78b2720933458e85b692e8ee0433d5
RSVictor <152220391+RSVictor@users.noreply.github.com>
2024-08-28 21:33:35 -0300

    Modificado o nome dos exercicios

---
+++ Aula5/ex15-objJason.js
@@ -0,0 +1,11 @@
+const dadoP = {
+    nome: 'Victor',
+    idade: 29, 
+    endereco:{
+        rua:'Av Amoreiras',
+        numero: 77,
+    }
+}
+
+console.log(dadoP)
+console.log(dadoP.endereco.rua)
