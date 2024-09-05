ceb9e7b3a9eaddce367597824055ec033be71845
Victor <>
2024-09-04 22:28:29 -0300

    Adicionado atividade 16

---
+++ Aula5/Atividades/Atividade16.js
@@ -0,0 +1,7 @@
+let pessoas = []
+
+pessoas.push({nome: "João", idade:29 , cidade:"Indaiatuba"})
+pessoas.push({nome: "Maria", idade:18 , cidade:"Campinas"})
+pessoas.push({nome: "Teodoro", idade:65 , cidade:"Hortolândia"})
+
+console.log(pessoas)
