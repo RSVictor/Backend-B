a86c92f437c490d2a47f3864829a11cffc58cae3
Victor <>
2024-09-04 22:13:16 -0300

    Adicionado atividade 15

--- Aula5/Atividades/Atividade15.js
+++ Aula5/Atividades/Atividade15.js
@@ -3,6 +3,11 @@ let pessoa = {
     idade : null,
     solteiro : null,
     hobbies : null,
+    endereco:{
+        rua: null,
+        cidade: null,
+        estado: null
+    },
     
     
 }
@@ -11,12 +16,18 @@ pessoa.nome = "Neymar Junior"
 pessoa.idade = "36"
 pessoa.solteiro = true
 pessoa.hobbies = "Enjoie parties"
+pessoa.endereco.rua =  "Avenida Amoreiras"
+pessoa.endereco.cidade = "Campinas"
+pessoa.endereco.estado = "São Paulo"
 
 function mostrarinfoPessoa(person) {
      console.log(person.nome)
      console.log(person.idade)
      console.log(person.solteiro)
      console.log(person.hobbies)
+     console.log(person.endereco.rua)
+     console.log(person.endereco.cidade)
+     console.log(person.endereco.estado)
 }
 
 mostrarinfoPessoa(pessoa)
