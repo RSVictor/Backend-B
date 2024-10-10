a2dd5857b2a0e52a561e208dbca9adaa8f95f31e
RSVictor <152220391+RSVictor@users.noreply.github.com>
2024-10-09 21:39:23 -0300

    Atualizar

--- Aula10/Library-Backend/server.js
+++ Aula10/Library-Backend/server.js
@@ -1,25 +1,31 @@
-const express = require('express');
-const mongoose =  require('mongoose');
+// arquivo principal da api
+// inicia o servidor
+require('dotenv').config() // Carrega as variaveis do arquivo venv
+const express = require('express')
+const mongoose = require('mongoose')
 const cors = require('cors')
+const bodyParser = require('body-parser');
+// inicialização do app
 
-// Inicialização do app
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const authRoutes = require('./routes/authRoutes')
+
 // Conexão ao MongoDB
-mongoose.connect('mongodb+srv://Victor:victor123@cluster0.ngnna.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect('mongodb+srv://Victor:victor123@cluster0.ngnna.mongodb.net/library', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB conectado'))
 .catch(err => console.log('Erro ao conectar ao MongoDB:', err));
 
-//Importação das rotas
-const booksRoutes = require('./routes/books');
-app.use('/api/books', booksRoutes);
-
-// Definir a porta do servidor
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000')
-})
+// Importação das rotas
+const bookRoutes = require('./routes/books');
+app.use('/api/books',bookRoutes); // irá retornar a rota dos livros
+app.use('/api/auth',authRoutes);
+// Define a porta do servidor
+app.listen(5000,()=>{
+    console.log('Servidor executando na porta 5000');
+});
